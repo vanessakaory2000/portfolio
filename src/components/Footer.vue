e466b769220a38d9a6b9593883f9887805ef9bdd
vanessakaory2000 <kurauchivanessa@gmail.com>
2025-05-28 13:01:48 -0300

    Delete src directory

--- src/components/Footer.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <footer class="fixed bottom-0 w-full bg-blue-900 text-white shadow p-6 flex justify-center items-center">
-        Todos os direitos reservados
-    </footer>
-  </template>
-
