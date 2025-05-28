53db0c803bedffa9ad3c1d786e8a5c4b38d2c256
vanessakaory2000 <kurauchivanessa@gmail.com>
2025-05-28 13:07:20 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <Navbar />
+    <main class="max-w-4xl mx-auto p-4 pb-24">
+      <router-view></router-view>
+    </main>
+    <Footer />
+  </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-12">
+    <h2 class="text-4xl font-semibold text-center mb-8 text-gray-800">Sobre Mim</h2>
+
+    <div class="mb-8">
+      <h3 class="text-2xl font-semibold text-gray-800 mb-4">Habilidades</h3>
+      <ul class="text-gray-700 space-y-2">
+        <li>Pacote Office</li>
+        <li>Linguagens: Python, C#, PHP</li>
+        <li>Desenvolvimento Web: Html, CSS</li>
+        <li>Banco de Dados: MySQL</li>
+      </ul>
+    </div>
+
+    <div class="mb-8">
+      <h3 class="text-2xl font-semibold text-gray-800 mb-4">Idiomas</h3>
+      <ul class="text-gray-700 space-y-2">
+        <li>Inglês - Básico</li>
+        <li>Japonês - Intermediário</li>
+      </ul>
+    </div>
+
+    <div class="mb-8">
+      <h3 class="text-2xl font-semibold text-gray-800 mb-4">Formação</h3>
+      <ul class="text-gray-700 space-y-2">
+        <li>Tecnologia em Alimentos - Fatec (Concluído em 2022)</li>
+        <li>Análise e Desenvolvimento de Sistemas - Unimar (2024 - atual)</li>
+      </ul>
+    </div>
+
+    <h3 class="text-2xl font-semibold text-gray-800 mb-4">Trajetória</h3>
+    <p class="mb-6 text-gray-700">2022 -> Estágio no laborátorio Físico/Químico da Fatec</p>
+  </section>
+</template> 
