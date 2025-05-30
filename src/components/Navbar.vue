f85e132ae5f57843c5d18ad76b882f898f9ee5c8
vanessakaory2000 <kurauchivanessa@gmail.com>
2025-05-29 23:40:23 -0300

    Add files via upload

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bg-blue-900 text-white shadow p-6 flex justify-between items-center">
-      <div class="text-xl font-bold">Meu Portfólio</div>
+      <div class="text-xl font-bold">Portfólio</div>
       <ul class="flex gap-6">
         <li><RouterLink to="/" class="hover:text-blue-600">Home</RouterLink></li>
         <li><RouterLink to="/about" class="hover:text-blue-600">Sobre</RouterLink></li>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,23 +2,23 @@
     <section class="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-12">
       <h2 class="text-4xl font-semibold text-center mb-8 text-gray-800">Contato</h2>
       
-      <form class="space-y-6">
-        <div>
+      <form class="space-y-6 flex flex-col gap-2 items-center w-full">
+        <div class="w-full">
           <label class="block text-lg font-semibold text-gray-700" for="nome">Nome</label>
-          <input id="nome" type="text" class="w-full px-4 py-3 mt-2 border rounded focus:ring-2 focus:ring-blue-300 focus:outline-none" placeholder="Digite seu Nome" />
+          <input id="nome" type="text" class="w-full px-4 py-3 mt-2 border-2 hover:border-blue-200 transition-colors duration-300 rounded focus:ring-2 focus:ring-blue-300 focus:outline-none" placeholder="Digite seu Nome" />
         </div>
   
-        <div>
+        <div class="w-full">
           <label class="block text-lg font-semibold text-gray-700" for="email">E-mail</label>
-          <input id="email" type="text" class="w-full px-4 py-3 mt-2 border rounded focus:ring-2 focus:ring-blue-300 focus:outline-none" placeholder="Digite seu E-mail" />
+          <input id="email" type="text" class="w-full px-4 py-3 mt-2 border-2 hover:border-blue-200 transition-colors duration-300 rounded focus:ring-2 focus:ring-blue-300 focus:outline-none" placeholder="Digite seu E-mail" />
         </div>
   
-        <div>
+        <div class="w-full">
           <label class="block text-lg font-semibold text-gray-700" for="mensagem">Mensagem</label>
-          <textarea id="mensagem" class="w-full px-4 py-3 mt-2 border rounded focus:ring-2 focus:ring-blue-300 focus:outline-none" rows="4" placeholder="Digite sua Mensagem"></textarea>
+          <textarea id="mensagem" class="w-full px-4 py-3 mt-2 border-2 hover:border-blue-200 transition-colors duration-300 rounded focus:ring-2 focus:ring-blue-300 focus:outline-none" rows="4" placeholder="Digite sua Mensagem"></textarea>
         </div>
   
-        <button type="submit" class="w-full bg-blue-600 text-white py-3 rounded hover:bg-blue-700">Enviar</button>
+        <button type="submit" class="w-1/2 mx-auto bg-white border border-blue-700 text-blue-700 hover:border-white hover:text-white hover:bg-blue-700 transition-all duration-500 py-3 rounded">Enviar</button>
       </form>
       
       <div class="mt-12 text-center">
